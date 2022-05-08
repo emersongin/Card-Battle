588f7ac031de858738d84b891dc012012b18ec21
Emerson Andrey <emersonandrey_7@hotmail.com>
2022-05-07 22:58:32 -0300

    feat: game_interpreter CardBattle plugin comand

--- js/plugins/CardBattle.js
+++ js/plugins/CardBattle.js
@@ -12,5 +12,15 @@
  */
 
 (function() {
-// include ./GameInterpreter.js 
+    const _Game_Interpreter_pluginCommand = Game_Interpreter.prototype.pluginCommand;
+
+    Game_Interpreter.prototype.pluginCommand = function(command, args) {
+        _Game_Interpreter_pluginCommand.call(this, command, args);
+
+        if (command === 'CardBattle') {
+            console.log('CardBattle Start');
+        }
+        
+    }  
 })();
+
---
+++ js/plugins/src/GameInterpreter.js
@@ -0,0 +1,10 @@
+    const _Game_Interpreter_pluginCommand = Game_Interpreter.prototype.pluginCommand;
+
+    Game_Interpreter.prototype.pluginCommand = function(command, args) {
+        _Game_Interpreter_pluginCommand.call(this, command, args);
+
+        if (command === 'CardBattle') {
+            console.log('CardBattle Start');
+        }
+        
+    } 
